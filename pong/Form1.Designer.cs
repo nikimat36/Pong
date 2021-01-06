8b2d0bb997de7df0bcc5256923f69fa05ccc65b0
nikimat36 <74063319+nikimat36@users.noreply.github.com>
2021-01-06 18:40:53 +0100

    Add files via upload

---
+++ pong/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace pong {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.cpu = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.playerScore = new System.Windows.Forms.Label();
+            this.cpuLabel = new System.Windows.Forms.Label();
+            this.endMsg = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Yellow;
+            this.player.Location = new System.Drawing.Point(12, 255);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(10, 88);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // cpu
+            // 
+            this.cpu.BackColor = System.Drawing.Color.Fuchsia;
+            this.cpu.Location = new System.Drawing.Point(778, 255);
+            this.cpu.Name = "cpu";
+            this.cpu.Size = new System.Drawing.Size(10, 88);
+            this.cpu.TabIndex = 1;
+            this.cpu.TabStop = false;
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.White;
+            this.Ball.Location = new System.Drawing.Point(401, 283);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(19, 18);
+            this.Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ball.TabIndex = 3;
+            this.Ball.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // playerScore
+            // 
+            this.playerScore.AutoSize = true;
+            this.playerScore.ForeColor = System.Drawing.Color.White;
+            this.playerScore.Location = new System.Drawing.Point(13, 13);
+            this.playerScore.Name = "playerScore";
+            this.playerScore.Size = new System.Drawing.Size(46, 17);
+            this.playerScore.TabIndex = 4;
+            this.playerScore.Text = "label1";
+            // 
+            // cpuLabel
+            // 
+            this.cpuLabel.AutoSize = true;
+            this.cpuLabel.ForeColor = System.Drawing.Color.White;
+            this.cpuLabel.Location = new System.Drawing.Point(721, 12);
+            this.cpuLabel.Name = "cpuLabel";
+            this.cpuLabel.Size = new System.Drawing.Size(46, 17);
+            this.cpuLabel.TabIndex = 5;
+            this.cpuLabel.Text = "label1";
+            // 
+            // endMsg
+            // 
+            this.endMsg.BackColor = System.Drawing.Color.Transparent;
+            this.endMsg.Font = new System.Drawing.Font("Arial Narrow", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.endMsg.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.endMsg.Location = new System.Drawing.Point(28, 256);
+            this.endMsg.Name = "endMsg";
+            this.endMsg.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.endMsg.Size = new System.Drawing.Size(744, 45);
+            this.endMsg.TabIndex = 6;
+            this.endMsg.Text = "label1";
+            this.endMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(845, 649);
+            this.Controls.Add(this.cpuLabel);
+            this.Controls.Add(this.playerScore);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.cpu);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.endMsg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox cpu;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label playerScore;
+        private System.Windows.Forms.Label cpuLabel;
+        private System.Windows.Forms.Label endMsg;
+    }
+}
+
---
+++ pong/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pong {
+    public partial class Form1 : Form {
+
+        bool goUp;
+        bool goDown;
+        int speed = 5;
+        int ballX = 5;  // horizontal speed
+        int ballY = 5;  // vertical speed
+        int score = 0;  //score for Player
+        int cpuPoint = 0;   // score for CPU
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up) {
+                //if pleyer press up key, 
+                //goUp is changed to true
+                goUp = true;    
+            }
+            if (e.KeyCode == Keys.Down) {
+                //if pleyer press down key, 
+                //goUp is changed to true
+                goDown = true;
+            }
+        }
+
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up) {
+                //if pleyer leaves up key, 
+                //goUp is changed to true
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down) {
+                //if pleyer leaves down key, 
+                //goUp is changed to true
+                goDown = false;
+            }
+
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            playerScore.Text = "" + score;
+            cpuLabel.Text = "" + cpuPoint;
+            endMsg.Text = "";
+
+            Ball.Top -= ballY;
+            Ball.Left -= ballX;
+
+            cpu.Top += speed;
+
+            //if cpu reached top/bottom, it will change direction
+            if (score < 5) {
+                if (cpu.Top < 0 || cpu.Top > 455) {
+                    speed = -speed;
+                }
+            //if the score is higher than 5, cpu will follow the ball
+            } 
+            if (score > 5){
+                cpu.Top = Ball.Top + 30;
+            }
+
+            //if ball  passes left side
+            if (Ball.Left < 0) {
+                Ball.Left = 434;    // reset ball to the middle
+                ballX = -ballX;     // change the ball direction
+                ballX -= 1;         // increase the speed
+                cpuPoint++;         // add 1 to the CPU score
+            }
+
+            //if ball passes the right side
+            if (Ball.Left + Ball.Width > ClientSize.Width) {
+                Ball.Left = 434;    // set ball to the centre
+                ballX = -ballX;     // change direction of the ball
+                ballX += 1;         // increase the speed of the ball
+                score++;            // add 1 to players score
+            }
+
+            //if ball reaches top/bottom of the screen, chenges direction
+            if (Ball.Top < 0 || Ball.Top + Ball.Height > ClientSize.Height) {
+                ballY = -ballY;
+            }
+
+            //if the ball hits cpu/player
+            if (Ball.Bounds.IntersectsWith(player.Bounds) || Ball.Bounds.IntersectsWith(cpu.Bounds)) {
+                ballX = -ballX;
+            }
+
+            //if player reaches top/bottom, he will move towards...
+            if (goUp == true && player.Top > 0) {       //the top
+                player.Top -= 8;
+            }
+            if (goDown == true && player.Top < 455) {   // the bottom
+                player.Top += 8;
+            }
+
+            //final score, ending game
+            if (score == 11) {
+                gameTimer.Stop();
+                endMsg.Text = "You win this game!!";
+                Console.Write("You win this game!!");
+            }
+
+            if (cpuPoint == 11) {
+                gameTimer.Stop();
+                endMsg.Text = "CPU wins, you lose";
+                
+                Console.Write("CPU wins, you lose.");
+            }
+        }
+
+    }
+}
